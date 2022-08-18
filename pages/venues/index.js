20feabe6e9859f15727d6f33300b1e17240b3f88
Ethan Teh <tehzl@hotmail.com>
2022-08-18 09:51:53 +0800

    Improve venue pages

--- pages/venues/index.js
+++ pages/venues/index.js
@@ -8,13 +8,6 @@ Page({
     data: {
 
     },
-
-    /**
-     * Lifecycle function--Called when page load
-     */
-    onLoad(options) {
-
-    },
     getData() {
         let page = this;
         wx.request({
@@ -38,9 +31,7 @@ Page({
           wx.event.on('loginFinish', page, page.getData)
         }
       },
-    /**
-     * Lifecycle function--Called when page is initially rendered
-     */
+
     onReady: {
         function (e) {
             // Use wx.createMapContext to obtain the map context
@@ -83,14 +74,14 @@ Page({
             })
           }
         },
-
-    /**
-     * Lifecycle function--Called when page show
-     */
-
-    /**
-     * Lifecycle function--Called when page hide
-     */
+    
+    goToShow(e) {
+        const id = e.currentTarget.dataset.index;
+        app.globalData.venue_id = id;
+        wx.switchTab({
+          url: '/pages/venues/show',
+        })
+    },
     onHide() {
 
     },
