858325f76b30306431f4fa4ad29fea3ddb071d83
Illarion Kopeika <larrypearson@yandex.ru>
2022-08-24 16:09:08 +0800

    Add form

--- pages/venues/index.js
+++ pages/venues/index.js
@@ -24,6 +24,7 @@ Page({
             method: 'GET',
             header: app.globalData.header,
             success(res) {
+                console.log('res grom GET venues: ', res.data)
                 page.storeData(res.data);
             }
         })
